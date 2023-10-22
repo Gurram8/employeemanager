0e67e27af25bdcefa41cf0257d88a8a3007c78e9
Gurram8 <bgurram8@gmail.com>
2023-10-21 20:04:54 -0500

    Added Update, Delete & View

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -8,11 +8,27 @@ import { Employee } from './employee';
 })
 export class EmployeeService {
 
-  private baseURL = "http://localhost:8080/api/v1/employees";
+  private baseURL = "http://localhost:8080/api/v1";
 
   constructor(private httpClient: HttpClient) { }
 
   getEmployeesList(): Observable<Employee[]> {
-    return this.httpClient.get<Employee[]>(`${this.baseURL}`);
+    return this.httpClient.get<Employee[]>(`${this.baseURL}/employees`);
+  }
+
+  createEmployee(employee: Employee): Observable<Object> {
+    return this.httpClient.post(`${this.baseURL}/create`, employee)
+  }
+
+  getEmployeeById(id: number): Observable<Employee> {
+    return this.httpClient.get<Employee>(`${this.baseURL}/employees/${id}`);
+  }
+
+  updateEmployee(id: number, employee: Employee): Observable<Object> {
+    return this.httpClient.put(`${this.baseURL}/update/${id}`, employee);
+  }
+
+  deleteEmployee(id: number): Observable<Object> {
+    return this.httpClient.delete(`${this.baseURL}/delete/${id}`);
   }
 }
